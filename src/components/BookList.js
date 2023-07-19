a0f1b58aee45acd1bb7aca6a1e3fb391c9166a0d
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-07-19 23:23:42 +0300

    fix addCase bugs

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,8 +3,8 @@ import { removeItem } from '../redux/books/bookSlice'
 import { useDispatch } from 'react-redux';
 // import {store} from '../redux/store'
 
-const BookList = ({ editBook, id, title, author , category}) => {
-const dispatch = useDispatch();
+const BookList = ({ editBook, id, title, author, category }) => {
+  const dispatch = useDispatch();
   const handleRemoveItem = (id) => {
     dispatch(removeItem(id));
   }
@@ -16,7 +16,7 @@ const dispatch = useDispatch();
         <li key={id}>
           <h2>{title}</h2>
           <p>{author}</p>
-          <p>{category }</p>
+          <p>{category}</p>
           <button
             onClick={() => handleRemoveItem(id)}
           >Remove</button>
